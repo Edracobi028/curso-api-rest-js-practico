792b957bb59e4a3619c142dbe207bfa3d6239122
DEV01ASM <erazo@agristar.com.mx>
2024-12-18 13:09:09 -0600

    Lista de películas en tendencia 5/17

--- src/main.js
+++ src/main.js
@@ -1,2 +1,34 @@
 
-console.log("API URL = http://dfgdfgfd.com/retrert?api_key=" + API_KEY);
+//console.log("API URL = http://dfgdfgfd.com/retrert?api_key=" + API_KEY);
+
+//Crear una funcion asincrona
+async function getTrendingMoviesPreview() {
+    
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY); //fetch asincrono + API KEY
+    const data = await res.json(); // Peticion asincrona que recibe data del fetch page y results
+    const movies = data.results;
+
+    //console.log('data= ', data, 'Results = ' , movies);
+    //Iterar para cargar con cada pelicula las tarjetas del index
+    movies.forEach(movie  => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList'); //apuntar al contenedor con el id y la clase
+        
+        const movieContainer = document.createElement('div'); //crear un div y 
+        movieContainer.classList.add('movie-container');  // agregar la clase del css movie-container'
+
+        const movieImg = document.createElement('img'); //crear un img 
+        movieImg.classList.add('movie-img');  // agregar la clase del css 
+        movieImg.setAttribute('alt',movie.title); //agregar atributo (tipo + valor) alt
+        movieImg.setAttribute(
+            'src',
+            'https://image.tmdb.org/t/p/w300' + movie.poster_path,); //agregar atributo (tipo + valor) alt
+        
+        //conectar los elementos creados al elemento del index
+        movieContainer.appendChild(movieImg); // meter la img al div
+        trendingPreviewMoviesContainer.appendChild(movieContainer); // meter el container a la seccion
+
+    });
+
+}
+
+getTrendingMoviesPreview(); //Llamar a la funcion
