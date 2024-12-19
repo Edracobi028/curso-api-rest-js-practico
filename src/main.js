1cbf92e682f43f5f8080129ace70c2e2f6418fa9
DEV01ASM <erazo@agristar.com.mx>
2024-12-19 11:31:18 -0600

    Lista de categorías 6/17

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 //console.log("API URL = http://dfgdfgfd.com/retrert?api_key=" + API_KEY);
 
-//Crear una funcion asincrona
+//Crear una funcion asincrona para obtener las peliculas en tendencia
 async function getTrendingMoviesPreview() {
     
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY); //fetch asincrono + API KEY
@@ -31,4 +31,35 @@ async function getTrendingMoviesPreview() {
 
 }
 
+async function getCategoriesPreview() {
+    
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY); //fetch asincrono + API KEY
+    const data = await res.json(); // Peticion asincrona que recibe data del fetch page y results
+    const categories = data.genres;
+
+    //console.log('data= ', data, 'Results = ' , movies);
+    //Iterar para cargar con cada pelicula las tarjetas del index
+    categories.forEach(category  => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list'); //apuntar al article con el id de section y la clase del article
+        
+        const categoryContainer = document.createElement('div'); //crear un div
+        categoryContainer.classList.add('category-container');  // agregar la clase del css que se le diseño'
+
+        
+        const categoryTitle = document.createElement('h3'); //crear un h3 
+        categoryTitle.classList.add('category-title');  // agregar la clase del css que se le diseño
+        categoryTitle.setAttribute('id', 'id' + category.id); //agregar atributo id (tipo + valor) 
+        
+        const categoryTitleText = document.createTextNode(category.name);
+
+        //conectar los elementos creados al elemento del index
+        categoryTitle.appendChild(categoryTitleText);  // meter el texto al h3
+        categoryContainer.appendChild(categoryTitle); // meter la h3 al div
+        previewCategoriesContainer.appendChild(categoryContainer); // meter el container a la seccion
+
+    });
+
+}
+
 getTrendingMoviesPreview(); //Llamar a la funcion
+getCategoriesPreview(); //Llamar a la funcion
